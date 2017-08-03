568d6df2bef3a9ac57f6fc005e7d5ab0956fdf69
cloud-elric <humberto@2gom.com.mx>
2017-08-03 12:25:22 -0500

    Compartir en facebook arreglado

--- views/site/ver-imagen.php
+++ views/site/ver-imagen.php
@@ -11,14 +11,61 @@ use yii\helpers\Url;
 <meta property="og:image" content="<?=Url::base()?>/fotos-tomadas/<?=$usuario->txt_imagen?>" />
 <meta property="og:url" content="<?=Url::base()?>/site/ver-imagen?token=<?=$usuario->txt_token?>" />
 
+<script>
+window.fbAsyncInit = function() {
+	FB.init({
+		//appId : '1029875693761281',
+		//appId:'171096896693553',
+		appId:'108260376517644',
+		cookie : true, // enable cookies to allow the server to access
+		// the session
+		xfbml : true, // parse social plugins on this page
+		version : 'v2.6' // use any version
+	});
+
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id))
+		return;
+	js = d.createElement(s);
+	js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+    function compartirFacebook() {
+
+	var image = "<?=Url::base()?>/site/ver-imagen?token=<?=$usuario->txt_token?>";
+	var description = "#campaña";
+	var title = "Mira mi increible fotografía";
+	
+	FB.ui({
+		method : 'feed',
+		name : title,
+		link : "<?=Url::base()?>/site/ver-imagen?token=<?=$usuario->txt_token?>",
+		picture : image,
+		caption : '2 Geeks one monkey',
+		description : description
+
+	}, function(response) {
+		if (response && response.post_id) {
+		} else {
+		}
+	});
+}
+</script>
 <title>Mi foto UFC</title>
 </head>
 
 <body>
 <img src="<?=Url::base()?>/fotos-tomadas/<?=$usuario->txt_imagen?>" />
-    <a href="https://www.facebook.com/sharer/sharer.php?<?=Url::base()?>/site/ver-imagen?token=<?=$usuario->txt_token?>" target="_blank">
-        Compartir en facebook
-    </a>
+   
+   <button onclick="compartirFacebook();">Compartir en facebook</button>
+
+
 </body>
 
 </html>
